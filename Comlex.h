d6fa637342ccaf77d60181f8e0e9c086f4868ee0
xuxu <2978615862@qq.com>
2023-02-24 21:47:36 +0800

    Complex

--- Comlex.h
+++ Comlex.h
@@ -23,4 +23,38 @@ private:
     double re, im; // 实部、虚部
 };
 
+
+Complex::Complex(double r, double i)
+{
+    re = r;
+    im = i;
+}
+Complex::Complex(const Complex& c)
+{
+    re = c.re;
+    im = c.im;
+}
+
+Complex& Complex::operator+=(const Complex& c)
+{
+    re += c.re;
+    im += c.im;
+    return *this;
+}
+
+Complex& Complex::operator-=(const Complex& c)
+{
+    re -= c.re;
+    im -= c.im;
+    return *this;
+}
+void Complex::print(const Complex& c)
+{
+    cout << c.re << "+" << c.im << endl;
+}
+
+ostream* Complex::operator<<(const Complex& c)
+{
+    cout << c.re << "+" << c.im << endl;
+}
 #endif
